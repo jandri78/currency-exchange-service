463c366a8ef751343069b08176107910a3bbb343
Andres Perez <jandri78@gmail.com>
2019-04-22 15:11:46 -0500

    JPA modification

--- src/main/java/com/andres/microservices/CurrencyExchangeController.java
+++ src/main/java/com/andres/microservices/CurrencyExchangeController.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,16 @@ public class CurrencyExchangeController {
 	@Autowired
 	private Environment enviroment;
 	
+	@Autowired
+	private ExchangeValueRepository repository;
+	
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from ,@PathVariable String to) {
 		
-		ExchangeValue exchangeValue =new ExchangeValue(100L,from,to,BigDecimal.valueOf(65));
+		//ExchangeValue exchangeValue =new ExchangeValue(100L,from,to,BigDecimal.valueOf(65));
+		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
+		
 		exchangeValue.setPort(Integer.parseInt(enviroment.getProperty("local.server.port")));
 		
 		
